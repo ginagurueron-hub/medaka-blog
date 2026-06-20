0be4275183a2cf5d2203f420400a2d5338d97b9f
ginagurueron-hub <ginaguru.eron@gmail.com>
2026-06-20 12:24:12 +0900

    Update generate_article.py

--- generate_article.py
+++ generate_article.py
@@ -114,7 +114,26 @@ def create_jekyll_post(article):
     front_matter += tags_str
     front_matter += "---\n\n"
 
-    return front_matter + article["body"]
+    affiliate_block = """
+---
+
+## 楽天市場でメダカ用品をさがす
+
+<!-- START MoshimoAffiliateEasyLink -->
+<script type="text/javascript">
+(function(b,c,f,g,a,d,e){b.MoshimoAffiliateObject=a;
+b[a]=b[a]||function(){arguments.currentScript=c.currentScript
+||c.scripts[c.scripts.length-2];(b[a].q=b[a].q||[]).push(arguments)};
+c.getElementById(a)||(d=c.createElement(f),d.src=g,
+d.id=a,e=c.getElementsByTagName("body")[0],e.appendChild(d))})
+(window,document,"script","//dn.msmstatic.com/site/cardlink/bundle.js?20220329","msmaflink");
+msmaflink({"n":"（めだか）（水草）おしゃれにテーブルで楽しむメダカ水槽セット　説明書付　飼育セット　本州四国限定","b":"","t":"","d":"https:\\/\\/thumbnail.image.rakuten.co.jp","c_p":"\\/@0_mall\\/chanet\\/cabinet\\/201","p":["\\/20106-1.jpg","\\/20106-2.jpg","\\/20106-3.jpg"],"u":{"u":"https:\\/\\/item.rakuten.co.jp\\/chanet\\/20106\\/","t":"rakuten","r_v":""},"v":"2.1","b_l":[{"id":1,"u_tx":"楽天市場で見る","u_bc":"#f76956","u_url":"https:\\/\\/item.rakuten.co.jp\\/chanet\\/20106\\/","a_id":5639267,"p_id":54,"pl_id":27059,"pc_id":54,"s_n":"rakuten","u_so":1}],"eid":"198wY","s":"s"});
+</script>
+<div id="msmaflink-198wY">リンク</div>
+<!-- MoshimoAffiliateEasyLink END -->
+"""
+
+    return front_matter + article["body"] + affiliate_block
 
 
 def save_post(content):
